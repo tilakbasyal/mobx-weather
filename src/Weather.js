2812c8180a3a0b6e5a6fdfdc6f5136f47a04b267
Tilak Basyal <basyaltilak143@gmail.com>
2019-12-20 22:40:09 +0545

    Finished except design

--- src/Weather.js
+++ src/Weather.js
@@ -1,47 +1,77 @@
 import React from "react";
 import { observer } from "mobx-react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Card from "@material-ui/core/Card";
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from "@material-ui/core";
+import clsx from "clsx";
+import useStyles from './styles'
+
+
+function StyledRadio(props) {
+  const classes = useStyles();
+
+  return (
+    <Radio
+      className={classes.root}
+      disableRipple
+      color="default"
+      checkedIcon={<span className={clsx(classes.icon, classes.checkedIcon)} />}
+      icon={<span className={classes.icon} />}
+      {...props}
+    />
+  );
+}
 
 export default observer(
   class People extends React.Component {
     componentWillMount() {
       this.props.store.loadWeather();
     }
-    // singleWeather = ({date, temp}) =>
-    //   <div key={temp}>
-    //     {/* <img src={`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`} alt="face" /> */}
-    //     {/* <p>Feels Like: {weather[0].main}</p> */}
-    //     <p>Avg Temprature: {temp}</p>
-    //     <p>Date: {date}</p>
-    //     <hr/>
-
-    //   </div>
-
     render() {
-      console.log(Object.keys(this.props.store.dateObject), "props");
       return (
-        <div>
-          <button
-            // onClick={this.props.store.loadPeople}
-            onClick={this.props.store.convertCelcius}
-          >
-            Celcius
-          </button>
-          <button
-          // onClick={this.props.store.loadPeople}
-          >
+        <React.Fragment>
+          <FormControl component='fieldset'>
+            <FormLabel component='legend'>Select Your Metric</FormLabel>
+            <RadioGroup
+              defaultValue='farenheiht'
+              aria-label='temperature'
+              name='customized-radios'
+            >
+              <FormControlLabel
+                value='farenheiht'
+                control={<StyledRadio />}
+                label='Farenheiht'
+                onClick={this.props.store.convertFahrenheit}
+              />
+              <FormControlLabel
+                value='celcius'
+                control={<StyledRadio />}
+                label='Celcius'
+                onClick={this.props.store.convertCelcius}
+              />
+            </RadioGroup>
+          </FormControl>
+          {/* <button onClick={this.props.store.convertCelcius}>Celcius</button>
+          <button onClick={this.props.store.convertFahrenheit}>
             Farenheiht
-          </button>
-          {/* {this.props.store.loading? "Loading..." : this.props.store.weather.map(this.singleWeather)} */}
-          {this.props.store.loading
-            ? "Loading..."
-            : Object.keys(this.props.store.dateObject).map(item => (
-                <div key={item}>
+          </button> */}
+          <div>
+            {this.props.store.loading ? (
+              <CircularProgress />
+            ) : (
+              Object.keys(this.props.store.freshObject).map(item => (
+                <Card key={item} style={{ margin: "16px", padding: "8px" }}>
                   <p>Date : {item}</p>
-                  <p>Average Temprature: {this.props.store.dateObject[item]}</p>
-                  <hr />
-                </div>
-              ))}
-        </div>
+                  <p>
+                    Average Temprature:{" "}
+                    {Number(this.props.store.freshObject[item]).toFixed(2)}
+                    {this.props.store.isFahrenheit ? "°F" : "°C"}
+                  </p>
+                </Card>
+              ))
+            )}
+          </div>
+        </React.Fragment>
       );
     }
   }
--- src/WeatherStore.js
+++ src/WeatherStore.js
@@ -3,23 +3,29 @@ import { runInAction, extendObservable, action } from "mobx";
 export default extendObservable(this, {
   weather: [],
   loading: true,
-  dateObject: [],
-  reducedObject: {},
+  freshObject: {},
+  initialTempInFar: [],
+  convertCelcius: null,
+  convertFahrenheit: null,
+  isFahrenheit: true,
+  weatherunit: 'imperial',
   loadWeather: action(async() => {
     this.loading = true;
-    const response = await fetch('http://api.openweathermap.org/data/2.5/forecast?q=Kathmandu&units=imperial&APPID=c7910a3f13b2a102322929da4f00d2ab&cnt=40');
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Kathmandu&units=imperial&APPID=c7910a3f13b2a102322929da4f00d2ab&cnt=40`);
     const json = await response.json();
     runInAction(() => {
       this.weather = json.list;
       this.loading = false;
 
-      // let dateArray = json.list.map(({dt_txt}) => {
-      //   let slicedDateArray = dt_txt.slice(0,10);
-      //   // slicedDateArray.reduce((acc, val) => acc.set(val, 1 + (acc.get(val) || 0)), new Map())
-      //   return slicedDateArray;
-      // })
+      let timeSegments = json.list.map(({dt_txt}) => {
+        let slicedDateArray = dt_txt.slice(0,10);
+        return slicedDateArray;
+      }).reduce((acc, value) => ({
+        ...acc,
+        [value]: (acc[value] || 0) + 1
+     }), {});
       
-      this.dateObject = json.list.map(({dt_txt, main}) => {
+      let dates = json.list.map(({dt_txt, main}) => {
         let slicedDateArray = dt_txt.slice(0,10);
         let newObj = {};
         newObj[slicedDateArray] = main.temp
@@ -27,23 +33,48 @@ export default extendObservable(this, {
       }).reduce((acc, val, index) => {
         acc[(Object.keys(val))] = acc[(Object.keys(val))] + Number(Object.values(val)) || Number(Object.values(val));
         return acc
-      },{})
-      
-      // this.reducedObject = dateObject.reduce((acc, val, index) => {
-      //   acc[(Object.keys(val))] = acc[(Object.keys(val))] + Number(Object.values(val)) || Number(Object.values(val));
-      //   return acc
-      // },{})
+      },{});
+      // console.log(Object.values(dates), 'temp')
+      // console.log(Object.keys(dates), 'dates')
+      // console.log(Object.values(timeSegments), 'times');
 
-      // console.log(dateArray, "date array");
-      // console.log(dateObject, "date Object");
-      // console.log(reduced, "reduced");
+      this.freshObject = Object.assign(...Object.keys(dates).map((v,i) => ({
+        [v]: (Object.values(dates)[i])/Object.values(timeSegments)[i]
+      })));
+
+      this.convertCelcius=(() => {
+        this.isFahrenheit= false,
+        this.freshObject = Object.assign(...Object.keys(dates).map((v,i) => ({
+          [v]: ((Number((Object.values(dates)[i])/Object.values(timeSegments)[i])-32)*5/9)
+        })))
+        console.log(Object.values(this.freshObject), "solved")
+    
+      }),
+      this.convertFahrenheit=(() => {
+        this.isFahrenheit= true,
+        this.freshObject = Object.assign(...Object.keys(dates).map((v,i) => ({
+          [v]: (Object.values(dates)[i])/Object.values(timeSegments)[i]
+        })))
+      })
+
+
+
+
+      // console.log(Object.values(this.freshObject), "solved")
     }); 
   }),
 
 
-  convertCelcius: action(() => {
-    // const {} = this.weather;
-    // console.log('i am celcius')
-  }
-  )
+  // convertCelcius: action(() => {
+  //   this.freshObject = Object.assign(...Object.keys(dates).map((v,i) => ({
+  //     [v]: ((Number((Object.values(dates)[i])/Object.values(timeSegments)[i])-32)*5/9)
+  //   })))
+  //   console.log(Object.values(this.freshObject), "solved")
+
+  // }),
+  // convertFahrenheit: action(() => {
+  //   this.freshObject = Object.assign(...Object.keys(dates).map((v,i) => ({
+  //     [v]: (Object.values(dates)[i])/Object.values(timeSegments)[i]
+  //   })))
+  // })
 });
