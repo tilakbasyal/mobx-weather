08f2c616bfe82979537913498f296e32db61b402
Tilak Basyal <basyaltilak143@gmail.com>
2019-12-20 13:33:16 +0545

    store to view

--- src/Weather.js
+++ src/Weather.js
@@ -6,31 +6,43 @@ export default observer(
     componentWillMount() {
       this.props.store.loadWeather();
     }
-    singleWeather = ({main, weather, dt_txt}) => 
-      <div key={dt_txt}>
-        <img src={`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`} alt="face" />
-        <p>Feels Like: {weather[0].main}</p>
-        <p>Avg Temprature: {main.temp}</p>
-        <p>Date: {dt_txt}</p>
-        <hr/>
+    // singleWeather = ({date, temp}) =>
+    //   <div key={temp}>
+    //     {/* <img src={`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`} alt="face" /> */}
+    //     {/* <p>Feels Like: {weather[0].main}</p> */}
+    //     <p>Avg Temprature: {temp}</p>
+    //     <p>Date: {date}</p>
+    //     <hr/>
 
-      </div>
-
+    //   </div>
 
     render() {
-      console.log(this.props.store.weather, 'props')
+      console.log(Object.keys(this.props.store.dateObject), "props");
       return (
-        <div >
-          <button 
+        <div>
+          <button
+            // onClick={this.props.store.loadPeople}
+            onClick={this.props.store.convertCelcius}
+          >
+            Celcius
+          </button>
+          <button
           // onClick={this.props.store.loadPeople}
-          onClick={this.props.store.convertCelcius}
-          >Celcius</button>
-          <button 
-          // onClick={this.props.store.loadPeople}
-          >Farenheiht</button>
-          {this.props.store.loading? "Loading..." : this.props.store.weather.map(this.singleWeather)}
+          >
+            Farenheiht
+          </button>
+          {/* {this.props.store.loading? "Loading..." : this.props.store.weather.map(this.singleWeather)} */}
+          {this.props.store.loading
+            ? "Loading..."
+            : Object.keys(this.props.store.dateObject).map(item => (
+                <div key={item}>
+                  <p>Date : {item}</p>
+                  <p>Average Temprature: {this.props.store.dateObject[item]}</p>
+                  <hr />
+                </div>
+              ))}
         </div>
-      )
+      );
     }
   }
-)
+);
--- src/WeatherStore.js
+++ src/WeatherStore.js
@@ -3,6 +3,8 @@ import { runInAction, extendObservable, action } from "mobx";
 export default extendObservable(this, {
   weather: [],
   loading: true,
+  dateObject: [],
+  reducedObject: {},
   loadWeather: action(async() => {
     this.loading = true;
     const response = await fetch('http://api.openweathermap.org/data/2.5/forecast?q=Kathmandu&units=imperial&APPID=c7910a3f13b2a102322929da4f00d2ab&cnt=40');
@@ -10,32 +12,31 @@ export default extendObservable(this, {
     runInAction(() => {
       this.weather = json.list;
       this.loading = false;
+
+      // let dateArray = json.list.map(({dt_txt}) => {
+      //   let slicedDateArray = dt_txt.slice(0,10);
+      //   // slicedDateArray.reduce((acc, val) => acc.set(val, 1 + (acc.get(val) || 0)), new Map())
+      //   return slicedDateArray;
+      // })
       
-      let dateArray = json.list.map(({dt_txt, main}) => {
+      this.dateObject = json.list.map(({dt_txt, main}) => {
         let slicedDateArray = dt_txt.slice(0,10);
-        // let counts = [slicedDateArray]
-        // console.log(slicedDateArray, 'slices')
         let newObj = {};
         newObj[slicedDateArray] = main.temp
         return newObj;
-      })
-      // .reduce((acc, val, index, array) => {
+      }).reduce((acc, val, index) => {
+        acc[(Object.keys(val))] = acc[(Object.keys(val))] + Number(Object.values(val)) || Number(Object.values(val));
+        return acc
+      },{})
+      
+      // this.reducedObject = dateObject.reduce((acc, val, index) => {
       //   acc[(Object.keys(val))] = acc[(Object.keys(val))] + Number(Object.values(val)) || Number(Object.values(val));
-      //   console.log(index,(Object.keys(val)), "Acc and val")
-      //   // console.log(String(Object.keys(val)),Number(Object.values(val)) , "Acc and val")
       //   return acc
       // },{})
 
-
-      let reduced = dateArray.reduce((acc, val, index) => {
-
-        acc[(Object.keys(val))] = acc[(Object.keys(val))] + Number(Object.values(val)) || Number(Object.values(val));
-        // console.log(index,(Object.keys(val)[0]),(Object.values(val)[0]), "Acc and val")
-        // console.log(String(Object.keys(val)),Number(Object.values(val)) , "Acc and val")
-        return acc
-      },{})
-
-        console.log([...dateArray], "Acc and it")
+      // console.log(dateArray, "date array");
+      // console.log(dateObject, "date Object");
+      // console.log(reduced, "reduced");
     }); 
   }),
 
