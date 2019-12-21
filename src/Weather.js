54bead20a34c56a925920e3b20586ed3139a1d8e
Tilak Basyal <basyaltilak143@gmail.com>
2019-12-21 22:51:15 +0545

    Trying to work on chart

--- src/Weather.js
+++ src/Weather.js
@@ -10,8 +10,11 @@ import {
   Radio,
   Grid,
   Typography,
-  Button
+  Button,
+  Divider
 } from "@material-ui/core";
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import clsx from "clsx";
 import useStyles from "./styles";
 // import './App.css';
@@ -31,40 +34,58 @@ function StyledRadio(props) {
   );
 }
 
-
 export default observer(
   class People extends React.Component {
-    constructor(props){
+    constructor(props) {
       super(props);
       this.state = {
-        start : 0,
-        end : 3
+        start: 0,
+        end: 3,
+        elemId: '',
+        hover: false
       };
       this.handleNext = this.handleNext.bind(this);
       this.handlePrevious = this.handlePrevious.bind(this);
-      }
+      this.handleCardClick = this.handleCardClick.bind(this);
+    }
 
-    componentWillMount() {
+    componentDidMount() {
       this.props.store.loadWeather();
     }
-    handleNext=() =>{
-      console.log(this.state, 'previous')
-
+    handleCardClick = (index) => {
+      console.log(index, "isdie card")
+    }
+    handleMouseEnter = (index) => {
+      // console.log(index, "index")
+      this.setState({ ...this.state,elemId: index, hover: true });
+    };
+  
+    handleMouseLeave = (index) => {
+      this.setState({ ...this.state,elemId: '', hover: false });
+    };
+    handleNext = () => {
+      // console.log(this.state, "previous");
       this.setState({
-        start : this.state.start + 1,
+        ...this.state,
+        start: this.state.start + 1,
         end: this.state.end + 1
-      })
-    }
-    handlePrevious=() =>{
+      });
+    };
+    handlePrevious = () => {
       this.setState({
-        start : this.state.start - 1,
+        ...this.state,
+        start: this.state.start - 1,
         end: this.state.end - 1
-      })
-    }
+      });
+    };
     render() {
-      const arrayLength = Object.keys(this.props.store.freshObject).length;
-      const mapper = Object.keys(this.props.store.freshObject).slice(this.state.start, this.state.end);
-      console.log(arrayLength, 'length')
+      // const arrayLength = Object.keys(this.props.store.freshObject).length;
+      const mapper = Object.keys(this.props.store.freshObject).slice(
+        this.state.start,
+        this.state.end
+      );
+      // console.log([...this.props.store.weather], "weather");
+      const { start, end, hover, elemId } = this.state;
       return (
         <React.Fragment>
           {this.props.store.loading ? (
@@ -81,7 +102,7 @@ export default observer(
           ) : (
             <React.Fragment>
               <FormControl component='fieldset' style={{ width: "100%" }}>
-                <FormLabel component='legend'>Select Your Metric</FormLabel>
+                <FormLabel component='legend' style={{width: '100%', textAlign:'center', fontSize:'2rem'}}>Select Your Metric</FormLabel>
                 <RadioGroup
                   defaultValue='farenheiht'
                   aria-label='temperature'
@@ -105,38 +126,59 @@ export default observer(
                   />
                 </RadioGroup>
               </FormControl>
-              <Grid container justify="space-between" style={{marginBottom: '16px'}}>
-                <Button 
-                variant = "contained"
-                color = "primary"
-                onClick={this.handlePrevious}
-                disabled = {this.state.start < 1 ? true : false}
-
-                >Previous</Button>
-                <Button 
-                variant = "contained"
-                color = "primary"
-
-                onClick={this.handleNext}
-                disabled = {this.state.end > 5  ? true : false}
-                >Next</Button>
+              <Divider style={{ margin: '1rem 0'}} />
+              <Grid
+                container
+                justify='space-between'
+                style={{ marginBottom: "16px" }}
+              >
+                <Button
+                  variant='contained'
+                  color='primary'
+                  onClick={this.handlePrevious}
+                  disabled={start < 1 ? true : false}
+                >
+                  <ArrowBackIosIcon />
+                </Button>
+                <Button
+                  variant='contained'
+                  color='primary'
+                  onClick={this.handleNext}
+                  disabled={end > 5 ? true : false}
+                >
+                  <ArrowForwardIosIcon />
+                </Button>
               </Grid>
-              <Grid container spacing={2}>
+              <Grid container spacing={2} justify="space-evenly">
                 {/* <div> */}
-                  {mapper.map(item => (
-                    <Grid style={{minWidth:220}} item xs={12} md={4} key={item}>
-                      <Card>
-                        <Typography>
-                          Date :<br /> {item}
-                        </Typography>
-                        <Typography>
-                          Average Temprature: <br />
+                {mapper.map((item,i) => (
+                  <Grid
+                    style={{ maxWidth: '320px' }}
+                    item
+                    xs={12}
+                    md={4}
+                    key={item}
+                    onClick={(i) => this.handleCardClick(i)}
+                  >
+                    <Card 
+                    onMouseLeave={() => this.handleMouseLeave(i)}
+                    onMouseEnter={() => this.handleMouseEnter(i)}
+                    raised={hover && elemId === i}
+                    style={{padding: '16px'}}
+                    >
+                      <Typography>
+                        Date :<br /> {item}
+                      </Typography>
+                      <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <Typography>Average Temprature:</Typography>
+                        <Typography variant='h3'>
                           {Number(this.props.store.freshObject[item]).toFixed(2)}
                           {this.props.store.isFahrenheit ? "°F" : "°C"}
                         </Typography>
-                      </Card>
-                    </Grid>
-                  ))}
+                      </div>
+                    </Card>
+                  </Grid>
+                ))}
                 {/* </div> */}
               </Grid>
             </React.Fragment>
--- src/WeatherStore.js
+++ src/WeatherStore.js
@@ -4,11 +4,9 @@ export default extendObservable(this, {
   weather: [],
   loading: true,
   freshObject: {},
-  initialTempInFar: [],
   convertCelcius: null,
   convertFahrenheit: null,
   isFahrenheit: true,
-  weatherunit: 'imperial',
   loadWeather: action(async() => {
     this.loading = true;
     const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Kathmandu&units=imperial&APPID=c7910a3f13b2a102322929da4f00d2ab&cnt=40`);
@@ -16,7 +14,7 @@ export default extendObservable(this, {
     runInAction(() => {
       this.weather = json.list;
       this.loading = false;
-
+      console.log(json.list, "i am weather")
       let timeSegments = json.list.map(({dt_txt}) => {
         let slicedDateArray = dt_txt.slice(0,10);
         return slicedDateArray;
@@ -43,24 +41,17 @@ export default extendObservable(this, {
       })));
 
       this.convertCelcius=(() => {
-        this.isFahrenheit= false,
+        this.isFahrenheit= false;
         this.freshObject = Object.assign(...Object.keys(dates).map((v,i) => ({
           [v]: ((Number((Object.values(dates)[i])/Object.values(timeSegments)[i])-32)*5/9)
-        })))
-        console.log(Object.values(this.freshObject), "solved")
-    
-      }),
+        })));
+      });
       this.convertFahrenheit=(() => {
-        this.isFahrenheit= true,
+        this.isFahrenheit= true;
         this.freshObject = Object.assign(...Object.keys(dates).map((v,i) => ({
           [v]: (Object.values(dates)[i])/Object.values(timeSegments)[i]
         })))
       })
-
-      this.initialTempInFar = Object.values(this.freshObject);
-
-
-      // console.log(this.initialTempInFar, "temp in far")
       // console.log(Object.values(this.freshObject), "solved")
     }); 
   }),
